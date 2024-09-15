4ea951c17cf5fc6002fc23a8ff9fea951f6e3b15
mjay <michael.a.jay@protonmail.com>
2024-09-15 13:55:54 -0400

    add recursive twelve days

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn gifts_for_day(day: &usize) -> impl Iterator<Item = &&'static str> {
     GIFTS.iter().take(day + 1).rev()
 }
 
-fn main() {
+fn iterative_twelve_days() {
     for (i, day) in DAYS.iter().enumerate() {
         println!("On the {} day of Christmas, my true love gave to me:", day);
         let mut gifts = gifts_for_day(&i).peekable();
@@ -35,4 +35,32 @@ fn main() {
         }
         println!();
     }
+}
+
+fn recursive_twelve_days(day: usize) {
+    if day >= DAYS.len() {
+        return;
+    }
+
+    println!(
+        "On the {} day of Christmas, my true love gave to me:",
+        DAYS[day]
+    );
+    let mut gifts = gifts_for_day(&day).peekable();
+    while let Some(gift) = gifts.next() {
+        let is_first_gift = gifts.peek().is_none();
+        if is_first_gift && day > 0 {
+            println!("and {}", gift);
+        } else {
+            println!("{}", gift);
+        }
+    }
+    println!();
+
+    recursive_twelve_days(day + 1)
+}
+
+fn main() {
+    // iterative_twelve_days();
+    recursive_twelve_days(0);
 }
